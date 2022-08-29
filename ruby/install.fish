716fb6dc9b34df99d19f0720c4f41605160033e7
Adam Melkus <adam.melkus@ysoft.com>
2022-08-29 12:33:38 +0200

    feat(ruby): Add rbenv

---
+++ ruby/install.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+switch (uname)
+case Darwin
+	if command -qs brew
+		brew install rbenv
+	end
+end
+
+set -gx PATH "$HOME.rbenv/shims" $PATH
+set -gx RBENV_SHELL fish
+command rbenv rehash 2>/dev/null
+function rbenv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case rehash shell
+    rbenv "sh-$command" $argv|source
+  case '*'
+    command rbenv "$command" $argv
+  end
+end
